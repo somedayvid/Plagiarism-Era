2d1699a9ec115c94f36e46c5233c3110d7632332
David Gao <david.04.gao@gmail.com>
2024-07-09 21:53:53 -0400

    quick grid update thats all

--- Zombie fighting plants/Grid.gd
+++ Zombie fighting plants/Grid.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 const GROUND_TILE = preload("res://ground_tile.tscn")
 var placementGrid = []
+@onready var tile_container = $"../TileContainer"
 
 const matrixLength := 9
 const matrixHeight := 5
 
-func _ready() -> void:
-	for i in matrixHeight:
-		var tempArray = []
-		placementGrid.append(tempArray)
-		for n in matrixLength:
-			placementGrid[i].append(1)
-	print(placementGrid)
+#func _ready() -> void:
+	#for i in matrixHeight:
+		#var tempArray = []
+		#placementGrid.append(tempArray)
+		#for n in matrixLength:
+			#var groundTileInstance = GROUND_TILE.instantiate()
+			#groundTileInstance.global_position = Vector2(n * 32, i * 32)
+			#tile_container.add_child(groundTileInstance)
+			#placementGrid[i].append(groundTileInstance)
+
+	#var count = -1
+	#for i in placementGrid:
+		#count += 1
+		#print(placementGrid[count])
