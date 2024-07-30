077ad25fbb799282a901f04f2bf1c3d23b9fb306
David Gao <david.04.gao@gmail.com>
2024-07-29 21:40:07 -0400

    zombie own script

--- Zombie fighting plants/enemies/HorizontalMovement.gd
+++ Zombie fighting plants/enemies/HorizontalMovement.gd
@@ -1,24 +1,11 @@
 extends CharacterBody2D
 
 @export var moveSpeed : float
-@export var rightDir := false
-var eating := false:
-	get:
-		return eating
-	set(value):
-		eating = value
-var released := false
-var type = "Zombie"
+var damage : int
 
 func _ready():
-	if rightDir:
-		velocity = Vector2(moveSpeed,0)
-	else:
-		velocity = Vector2(-moveSpeed,0)
+	velocity = Vector2(moveSpeed,0)
 
 func _physics_process(delta):
-	if !eating:
-		move_and_slide()
+	move_and_slide()
 
-func _on_release_timer_timeout():
-	released = true
